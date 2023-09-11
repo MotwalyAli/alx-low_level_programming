671b0ed4877b51d7e9f0d3d556fda7d8f56a02ff
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-11 15:49:40 -0700

    task-6 done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this main is very important
+ * this is not like the above as it seems to be
+ * Return: 0
+ */
+int main(void)
+{
+int Nu;
+
+for (Nu = 48; Nu < 58; Nu++)
+{
+putchar(Nu);
+}
+putchar('\n');
+return (0);
+}
