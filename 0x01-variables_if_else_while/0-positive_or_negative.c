3d5380e2cf28ec6c0ae016463120d4fc5d1c80d4
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-11 13:10:04 -0700

    if elseif else program

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - is the start key for the program
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+/*(for the positive message.*/
+if (n > 0)
+{
+	printf("%d is positive\n", n);
+}
+/*for the negative message.*/
+else if (n < 0)
+{
+	printf("%d is negatice\n", n);
+}
+/*for anything else which is the 0*/
+else
+{
+	printf("%d is zero\n", n);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,11 @@
+                       if_else_while Readme file
+this readme file is made for 0x01-variables_if_else_while
+
+
+	->>GORGE BOOLE
+
+- Gorge Boole is the faster of the computers because he creat the Boole Logic which is used by Alan Turing to inventor [Enigma machine].Boolean logic made the 0&1 computer  systems and the logic gates.
+
+	->>BOOLEAN OPERATORS
+
+- 
