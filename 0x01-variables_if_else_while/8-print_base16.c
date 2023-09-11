ce31c29fe2c5e43500efa357708e34f031ef182f
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-11 16:08:44 -0700

    task-8 dane

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this the main which is important
+ * print base16 numbers
+ * Return: 0
+ */
+int main(void)
+{
+int Nu;
+char Ch;
+
+for (Nu = 48; Nu < 58; Nu++)
+{
+putchar(Nu);
+}
+for (Ch = 'a'; Ch <= 'f'; Ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
