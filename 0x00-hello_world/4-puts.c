dca5222fb6f58abbc312f32b324a58021f431ce0
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-11 05:17:14 -0700

    solving task-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,6 @@
-/*************************************************
-->this is my first program ever in C language
-->the first line of this program is the first line of any program in C whisch indicate the libray.
-->the second line the key of exectution and without defining the main() function the machine can not even knew that there is a C source code.
-->this program specifically around the puts command which prints the value or whatever inside its prensethies and has no any printing format option.
-**************************************************/
 #include <stdio.h>
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle);
-    
 return (0);
 }
