bb6f55787429b180ab5a9294b6c58eee269c3590
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-11 16:40:43 -0700

    fix 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 int di1, di2;
 
-for (di = 0; di < 9; di++)
+for (di1 = 0; di1 < 9; di1++)
 {
-for (di = di1 + 1; di < 10; di2++)
+for (di2 = di1 + 1; di2 < 10; di2++)
 {
 putchar((di1 % 10) + '0');
 putchar((di2 % 10) + '0');
 
-if (di1 == 8 && di == 9)
+if (di1 == 8 && di2 == 9)
 continue;
 
 putchar(',');
