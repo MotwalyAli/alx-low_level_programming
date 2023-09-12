c1fa96afebaff26ac361548e1990a66ab4c21ef4
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-12 06:12:37 -0700

    printing without stdlib using putchar t-0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is key to open our program 
+ * this program is to print _putchar without the use of stdlib funtions
+ * Return: 0
+ */
+int main(void)
+{
+
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,7 @@
+                                         Functions_Nested_Loops README file
+ 
+   ->> NOTE:
+- In this project we are not allowed to use [global variables] or to use the standard library fuctions like [printf, puts, etc..] but we are going use the putchar fuction for the first task.
+
+
+
