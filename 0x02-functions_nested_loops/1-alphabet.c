ad62dda8d284f3921de66f30d061f0bb063fae97
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-12 12:00:34 -0700

    solve t-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ * void - is the most importat thing
+ * this program prints lowercase alphabets
+ * Return: 0
+*/
+void print-alphabet(void)
+{
+char letter = 'a';
+/*looping untill reaching z letter*/
+while (letter <= 'z')
+{
+/*printing the letters using putchar*/
+_putchar(letter);
+/*increasing letters from a to z*/
+letter++;
+}
+_putchar('\n');
+return (0);
+
+}
