540e39ce9fe538a4af5d284a3db54b5806625f2c
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-12 19:36:29 -0700

    solve or fix

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - this is our function
+ *
+ * @a: operand for the function
+ * @b: second opernad for the function
+ *
+  * Return: sum of both integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
