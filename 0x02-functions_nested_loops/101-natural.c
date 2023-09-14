7b7b0842d2a788b2f50df75c335e9ceb2a372960
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-13 17:29:13 -0700

    solve t-13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main func
+ * Return: 0 at the end of the for loop
+*/
+int main(void)
+{
+/*Variable to store the sumation of the multibules of 3 and 5*/
+int sum = 0;
+
+for (int n = 1; n < 1024; n++)
+{
+	if (n % 3 == 0 || n % 5 == 0)
+	{
+/* Add the number to the sum each time when the condition is true*/
+	sum = sum + n;
+	}
+}
+/*Print the sum of the multibules*/
+printf("%d\n", sum);
+return (0);
+}
