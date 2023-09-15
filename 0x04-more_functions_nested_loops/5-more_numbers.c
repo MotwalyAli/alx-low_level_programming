8695417d2ce77006975d2d4b00e7af6f491fa678
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-14 18:42:24 -0700

    solve t-5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints more numbers
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
