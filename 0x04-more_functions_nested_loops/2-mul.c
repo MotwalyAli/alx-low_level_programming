e1eb2389fe8f17c41ae86dd4cd52e4106297c028
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-14 17:55:12 -0700

    solve t-2 a progarm gives result of multipling a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - determine if digit
+ *
+ * @a: the first character to multiplied
+ * @b: the second operand in the multiplication process
+ *
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
