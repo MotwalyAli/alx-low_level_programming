ec4e1aa92f5465467bee9bf3c389f166e561fa4a
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-14 18:08:59 -0700

    solve t-3 to print num from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - this is the main function
+ *
+ * return: 0
+ *
+*/
+void print_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+	{
+	_putchar(n);
+	}
+_putchar('\n')
+return (0);
+}
