f17606f218c54fb9a3dd96f25c021233ebe998bc
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-18 14:45:26 -0700

    pace problems

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
  * reset_to_98 - this function to change the value of the int pointed to
- * 
- * @*n: is the pointer pointing to the value of n
+ *
+ * @n: is the pointer pointing to the value of n
 */
 void reset_to_98(int *n)
 {
