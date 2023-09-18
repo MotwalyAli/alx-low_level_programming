5ef89916ffcbe2607782b9dfa3b222e9ddc1c5aa
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-18 15:15:08 -0700

    solve t-2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - this function to change the value of the int pointed to
+ *
+ * @s: is the pointer pointing to the value of s
+ * return the num of string charcters
+*/
+int _strlen(char *s)
+{
+int i ;
+for (i = 0; s[i] != '\0'; i++)
+{
+return (i);
+}
+}
