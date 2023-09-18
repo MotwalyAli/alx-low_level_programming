38439d76fe4a7ae2c40e7adf7edd8379f13a90f4
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-18 15:00:23 -0700

    solve t-1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - this function to swap the value of the pointers a & b
+ *
+ * @a: is the pointer pointing to the value of a which is i
+ * @b: is the pointer pointing to the value of b which is j
+ *
+*/
+void swap_int(int *a, int *b)
+{
+int *a = i;
+int *b = j;
+
+*a = j;
+*b = i;
+}
