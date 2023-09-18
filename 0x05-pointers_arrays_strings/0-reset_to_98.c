7b05f15c049c92dcc9cf252eb2f1116d7e4ca790
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-18 14:41:22 -0700

    commiting the first task t-0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function to change the value of the int pointed to
+ * 
+ * @*n: is the pointer pointing to the value of n
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+                                     Pointers_Arrays_Strings README.md file
+    ->>Note:
+
+- Theis file made specificly for the Pointers & Arrays project (0x05.
