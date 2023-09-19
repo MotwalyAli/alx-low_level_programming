bf1697bc5ef7caa9a911218823d18b9a2141e06f
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-19 11:12:30 -0700

    solve t-3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - this is our main function to put a string
+ *
+ * @str: is the string will printed
+ */
+void _puts(char *str)
+{
+int n;
+
+for (n = 0; *(str + n) != '\0'; n++)
+{
+_putchar(*(str + n));
+}
+_putchar('\n');
+}
