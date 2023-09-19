7333b61161ab6b4a84bb66f877fb9d099cf375d4
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-19 15:24:56 -0700

    solve t-5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * rev_string - main function to reverse a string
+ *
+ * @s: the string which is going to be printed reversly 
+ */
+void rev_string(char *s)
+{
+	int i;
+	char t;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		t = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = t;
+	}
+}i
