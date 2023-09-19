593463c7df7b271bc42b1c924ff31f415e350c0c
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-09-19 15:42:20 -0700

    solve t-7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - this is our main function to print half of the screan
+ *
+ * @str: the string intered which is going to be used
+ */
+void puts_half(char *str)
+{
+	int n;
+	int oE = 0;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		oE += 1;
+	}
+	for (a = (_strlen(str) + oE) / 2; a < _strlen(str); a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
