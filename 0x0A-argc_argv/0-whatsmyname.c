18ae4a236cc76b0af34354b9d707fc9101becbc7
Mohammed Motwaly Ali <motwalyali8899@gmail.com>
2023-10-09 18:42:18 -0700

    solving the first tast

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function with two arguments to be used
+ * @argc: is the command line argument counter with integer datatype
+ * @argv: is apointer of the command line argument vactor with char datatype
+ *
+ * Return: (0) at the end of the program.
+ */
+int main(int argc, char **argv)
+{
+(void)argc;
+printf(" %d", argv[0]);
+printf("\n");
+return (0);
+}
